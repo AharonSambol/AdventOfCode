870cd419c41d47db2d424941b2aadab26eb52586
Aharon Sambol <aharon.sambol@gmail.com>
2020-12-02 22:41:10 +0200

    shorened a bit

--- 2020/day02.go
+++ 2020/day02.go
@@ -31,12 +31,9 @@ func part1() {
 	for _, line := range input {
 		//splits the string by the regex (i know this is bad)
 		lineArr := strings.Split(r.ReplaceAllString(line, ","), ",")
-		num1, err := strconv.Atoi(lineArr[0]) //turn to int
-		if err != nil {
-			log.Fatalf("char isnt number")
-		}
-		num2, err := strconv.Atoi(lineArr[1]) //turn to int
-		if err != nil {
+		num1, err1 := strconv.Atoi(lineArr[0]) //turn to int
+		num2, err2 := strconv.Atoi(lineArr[1]) //turn to int
+		if err1 != nil || err2 != nil {
 			log.Fatalf("char isnt number")
 		}
 		//count how many times the char appears in the string
@@ -55,14 +52,12 @@ func part2() {
 	for _, line := range input {
 		//splits the string by the regex (i know this is bad)
 		lineArr := strings.Split(r.ReplaceAllString(line, ","), ",")
-		num1, err := strconv.Atoi(lineArr[0]) //turn to int
-		if err != nil {
+		num1, err1 := strconv.Atoi(lineArr[0]) //turn to int
+		num2, err2 := strconv.Atoi(lineArr[1]) //turn to int
+		if err1 != nil || err2 != nil {
 			log.Fatalf("char isnt number")
 		}
-		num2, err := strconv.Atoi(lineArr[1]) //turn to int
-		if err != nil {
-			log.Fatalf("char isnt number")
-		}
+
 		lineR := []rune(lineArr[3]) // turn the line of characters into an array of characters
 		//check that theyre different and one of them equals the wanted character
 		if strings.Count(string(lineR[num1-1])+string(lineR[num2-1]), lineArr[2]) == 1 {
