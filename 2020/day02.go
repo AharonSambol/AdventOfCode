d510ddfbd6627e7d42816fe3358c27845188b189
Aharon Sambol <aharon.sambol@gmail.com>
2020-12-04 15:13:50 +0200

    read from file

--- 2020/day02.go
+++ 2020/day02.go
@@ -1,31 +1,11 @@
 package main
 
 import (
-	"bufio"
 	"log"
-	"os"
 	"regexp"
 	"strconv"
 	"strings"
 )
-
-func main() {}
-func ReadFromFile(fileName string) []string {
-	file, err := os.Open(fileName)
-
-	if err != nil {
-		log.Fatalf("failed to open")
-	}
-	scanner := bufio.NewScanner(file)
-	scanner.Split(bufio.ScanLines)
-	var text []string
-	for scanner.Scan() {
-
-		text = append(text, scanner.Text())
-	}
-	file.Close()
-	return text
-}
 
 func part1() {
 	input := ReadFromFile("day02.txt")
