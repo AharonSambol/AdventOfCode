def8f705062eb19027fbe718ce323b380186b9ec
Aharon Sambol <aharon.sambol@gmail.com>
2020-12-05 20:00:27 +0200

    day5 and one line day 4

--- 2020/day04.py
+++ 2020/day04.py
@@ -1,4 +1,6 @@
 import re
+
+
 def readFromFile(file):
     input = []
     with open(file) as in_file:
@@ -6,6 +8,7 @@ def readFromFile(file):
             input.append(line)
     input.append("\n")
     return input
+
 
 def day4part1():
     count = 0
@@ -20,19 +23,35 @@ def day4part1():
         passport += i
     print(count)
 
-def day4part2():
+
+# this one is longer but more readable
+def day4part2option1():
     count = 0
-    d = {}
-    # 1 split by (\r?\\n){2}
+    dic = {}
     with open("day04.txt") as in_file:
         for line in in_file:
             if line == "\n":
-                count += check_valid(d)
-                d = {}
+                count += check_valid(dic)
+                dic = {}
                 continue
-            for value in line.replace("\n","").split(" "):
-                d[value.split(":")[0]] = value.split(":")[1]
+            for value in line.replace("\n", "").split(" "):
+                dic[value.split(":")[0]] = value.split(":")[1]
 
+    print(count)
+
+
+# this one is shorter but harder to understand
+def day4part2option2():
+    count = 0
+    st = ""
+    with open("day04.txt") as in_file:
+        for line in in_file:
+            if line == "\n":
+                if check_valid2(st):
+                    count += 1
+                st = ""
+                continue
+            st += line.replace("\n", "")
     print(count)
 
 
@@ -49,4 +68,9 @@ def check_valid(d):
     return 0
 
 
-day4part2()
+def check_valid2(d):
+    return re.match(".*(?=.*(byr:200[0-2]|byr:19[2-9][0-9]))(?=.*(iyr:201[0-9]|iyr:2020))(?=.*(eyr:202[0-9]|eyr:2030))(?=.*(hgt:1[5-8][0-9]cm|hgt:19[0-3]cm|hgt:6[0-9]in|hgt:59in|hgt:7[0-6]in))(?=.*hcl:#[0-9a-f]{6})(?=.*ecl:(amb|blu|brn|gry|grn|hzl|oth))(?=.*pid:[0-9]{9}[a-zA-Z\s]).*", d+" ")
+
+
+day4part2option1()
+day4part2option2()
---
+++ 2020/day05.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"sort"
+)
+
+func main() {
+	day5pt1()
+}
+func ReadFromFile(fileName string) []string {
+	file, err := os.Open(fileName)
+
+	if err != nil {
+		log.Fatalf("failed to open")
+	}
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	var text []string
+	for scanner.Scan() {
+
+		text = append(text, scanner.Text())
+	}
+	file.Close()
+	return text
+}
+func day5pt1() {
+	input := ReadFromFile("day05.txt")
+	seats := []int{}
+	for _, pass := range input {
+		row := []int{0, 127}
+		col := []int{0, 7}
+		for _, ch := range pass {
+			switch ch {
+			case 'F':
+				row[1] = row[0] + (row[1]-row[0])/2
+				break
+			case 'B':
+				row[0] = row[1] - (row[1]-row[0])/2
+				break
+			case 'L':
+				col[1] = col[0] + (col[1]-col[0])/2
+				break
+			case 'R':
+				col[0] = col[1] - (col[1]-col[0])/2
+				break
+			}
+		}
+		seats = append(seats, row[0]*8+col[0])
+	}
+	sort.Ints(seats)
+	//part 1
+	println(seats[len(seats)-1])
+	//part 2
+	for i, seat := range seats {
+		if seats[i+1] != seat+1 {
+			println(seat + 1)
+			break
+		}
+	}
+
+}
