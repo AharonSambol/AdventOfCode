5cff820b271410ff29e5da058f526c4a5b7bb9be
Aharon Sambol <aharon.sambol@gmail.com>
2020-12-08 18:02:07 +0200

    cleaned up day 7

--- 2020/PythonAnswers/day07.py
+++ 2020/PythonAnswers/day07.py
@@ -11,22 +11,25 @@ def day7(part_one):
     input = read_input_chunks()
     hm = {}
     for line in input:
-        thisBag = line.split("s contain")[0]
+        thisBag = line.split(" bags contain")[0]
         if "no other bags" in line:
             hm[thisBag] = []
             continue
-        allBags = []
-        for i, bag in enumerate(re.split("(.*contain|, |\\.)", line)):
-
-            if not re.match(".*(, |\\.|contain|\n|^$).*", bag):
-                amount = int(re.findall("\\d", bag)[0])
+        ListOfBags = []
+        allBags = re.split("(?:.*contain (\\d) | bags?, (\\d) | bags?\\.\\n?)", line)
+        allBags = list(filter(lambda x: x != "" and x is not None, allBags))
+        amount = 0
+        for i, bag in enumerate(allBags):
+            if bag.isnumeric():
+                amount = int(bag)
+            else:
                 for _ in range(amount):
-                    allBags.append(bag.strip().replace("bags", "bag")[len(str(amount))+1:])
-        hm[thisBag] = allBags
+                    ListOfBags.append(bag)
+        hm[thisBag] = ListOfBags
     if part_one:
-        print(find_all(hm, "shiny gold bag", 0))
+        print(find_all(hm, "shiny gold", 0))
     else:
-        print(find_amount(hm, "shiny gold bag", 0))
+        print(find_amount(hm, "shiny gold", 0))
 
 
 def find_all(d, color, count):
@@ -43,4 +46,4 @@ def find_amount(d, color, count):
     return count
 
 
-day7(False)
+day7(True)
