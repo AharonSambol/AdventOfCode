1cd4f8a9443a943faf79d288b32f46b1d456e12c
Aharon Sambol <aharon.sambol@gmail.com>
2020-12-08 18:28:37 +0200

    combined both parts to 1 function

--- 2020/GoAnswers/day08.go
+++ 2020/GoAnswers/day08.go
@@ -6,49 +6,21 @@ import (
 	"strings"
 )
 
-func day8pt1() {
-	m := make(map[int]bool)
-	count := 0
-	line := 0
-	input := ReadFromFile("day08.txt")
-	for {
-		instruction := input[line]
-		sp := strings.Split(instruction, " ")
-		typ, valSt := sp[0], sp[1]
-		if _, ok := m[line]; ok {
-			print(count)
-			break
-		}
-		m[line] = true
-		val, _ := strconv.Atoi(valSt)
-		switch typ {
-		case "nop":
-			line++
-			break
-		case "acc":
-			line++
-			count += val
-		case "jmp":
-			line += val
-		}
-	}
-}
-
-func rec(m map[int]bool, line int, count int, input []string, changed bool) {
+func rec(m map[int]bool, line int, count int, input []string, partTwo bool, changed bool) {
 	for {
 		instruction := input[line]
 		sp := strings.Split(instruction, " ")
 		typ, valSt := sp[0], sp[1]
 		val, _ := strconv.Atoi(valSt)
 		if _, ok := m[line]; ok {
+			println(count) // part one
 			break
 		}
-
 		switch typ {
 		case "nop":
-			if !changed {
+			if partTwo && !changed {
 				input[line] = "jmp " + valSt
-				go rec(copyMap(m), line, count, input, true)
+				go rec(copyMap(m), line, count, input, true, true)
 			}
 			m[line] = true
 			line++
@@ -57,23 +29,21 @@ func rec(m map[int]bool, line int, count int, input []string, changed bool) {
 			line++
 			count += val
 		case "jmp":
-			if !changed {
+			if partTwo && !changed {
 				input[line] = "nop " + valSt
-				go rec(copyMap(m), line, count, input, true)
+				go rec(copyMap(m), line, count, input, true, true)
 			}
 			m[line] = true
 			line += val
 		}
-
-		if line >= len(input) {
-			println("!", count)
+		if partTwo && line >= len(input) {
+			println("!", count) //part two
 			os.Exit(3)
 		}
 	}
-	for {
+	for partTwo {
 	}
 }
-
 func copyMap(originalMap map[int]bool) map[int]bool {
 	newMap := make(map[int]bool, 0)
 	for k, v := range originalMap {
@@ -82,5 +52,5 @@ func copyMap(originalMap map[int]bool) map[int]bool {
 	return newMap
 }
 func day8pt2() {
-	rec(make(map[int]bool), 0, 0, ReadFromFile("day08.txt"), false)
+	rec(make(map[int]bool), 0, 0, ReadFromFile("day08.txt"), true, false)
 }
