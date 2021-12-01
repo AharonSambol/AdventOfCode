7ffe360e74a7e10f416acefab12cd5681261a034
AharonSambol <75334174+AharonSambol@users.noreply.github.com>
2021-12-01 12:07:27 +0200

    Update Day1.py

--- 2021/PythonAnswers/Day1.py
+++ 2021/PythonAnswers/Day1.py
@@ -1,7 +1,7 @@
 with open("day1") as input_file:
     lines = list(map(int, input_file.readlines()))
     # part 1
-    print(sum(1 for prev, num in zip(lines, lines[1:]) if num > prev))
+    print(sum(num > prev for prev, num in zip(lines, lines[1:])))
 
     # part 2
-    print(sum(1 for num1, num4 in zip(lines, lines[3:]) if num1 < num4))
+    print(sum(num1 < num4 for num1, num4 in zip(lines, lines[3:])))
