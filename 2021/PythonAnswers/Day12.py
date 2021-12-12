cf9511d89f97cb759a535918fbe666feb7c343c0
AharonSambol <75334174+AharonSambol@users.noreply.github.com>
2021-12-12 19:10:21 +0200

    Update Day12.py

--- 2021/PythonAnswers/Day12.py
+++ 2021/PythonAnswers/Day12.py
@@ -1,22 +1,21 @@
-amount = 0
-def calc(from_cave, path, visited_twice):
-    global amount
+def calc(caves, from_cave, path, visited_twice):
     if from_cave == 'end':
-        amount += 1
-        return
+        return 1
+    amount = 0
     for dest in caves.get(from_cave, []):
         if dest.islower() and (path.get(dest, 0) == 2 or (path.get(dest, 0) == 1 and visited_twice)):
             continue
         path[from_cave] = path.get(from_cave, 0) + 1
-        calc(dest, path, visited_twice or (dest.islower() and path.get(dest, 0) == 1))
+        amount += calc(caves, dest, path, visited_twice or (dest.islower() and path.get(dest, 0) == 1))
         path[from_cave] -= 1
+    return amount
 
 
-caves = dict()
-with open("input.txt") as input_file:
-    for line in input_file.readlines():
-        from_cave, to_cave = line.strip().split("-")
-        for from_cave, to_cave in [(from_cave, to_cave),  (to_cave, from_cave)]:
+if __name__ == '__main__':
+    caves = dict()
+    with open("day12") as input_file:
+        for line in input_file.readlines():
+            from_cave, to_cave = line.strip().split("-")
             caves[from_cave] = caves.get(from_cave, []) + [to_cave]
-    calc('start', {'start': 1}, False)
-    print(amount)
+            caves[to_cave] = caves.get(to_cave, []) + [from_cave]
+    print(calc(caves, 'start', {'start': 1}, False))
