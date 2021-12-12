0a37e4a73824dab41e1a5e10ab5871f19eb0781f
AharonSambol <75334174+AharonSambol@users.noreply.github.com>
2021-12-12 11:44:07 +0200

    Update Day10.py

--- 2021/PythonAnswers/Day10.py
+++ 2021/PythonAnswers/Day10.py
@@ -1,26 +1,20 @@
-def day10(ipt):
-    part1_ans, part2_ans = 0, []
-    values = {')': (3, 1), ']': (57, 2), '}': (1197, 3), '>': (25137, 4)}
-    opposites = {'(': ')', '[': ']', '{': '}', '<': '>'}
-    for line in ipt:
-        stack = []
-        for char in line:
-            if char in values:
-                if opposites[stack[-1]] == char:
-                    stack.pop(-1)
-                else:
-                    part1_ans += values[char][0]
-                    break
-            else:
-                stack.append(char)
-        else:
-            part2_ans.append(0)
-            for char in stack[::-1]:
-                part2_ans[-1] = part2_ans[-1] * 5 + values[opposites[char]][1]
-    print(f"part1: { part1_ans }")
-    print(f"part2: { sorted(part2_ans)[len(part2_ans) // 2] }")
-
+def calc(caves, from_cave, path, visited_twice):
+    amount = 0
+    if from_cave == 'end':
+        return 1
+    for dest in caves.get(from_cave, []):
+        if dest.islower() and (path.get(dest, 0) == 2 or (path.get(dest, 0) == 1 and visited_twice)):
+            continue
+        path[from_cave] = path.get(from_cave, 0) + 1
+        amount += calc(caves, dest, path, visited_twice or (dest.islower() and path.get(dest, 0) == 1))
+        path[from_cave] -= 1
+    return amount
 
 if __name__ == '__main__':
-    with open("day10") as input_file:
-        day10([x.strip() for x in input_file.readlines()])
+    caves = dict()
+    with open("input.txt") as input_file:
+        for line in input_file.readlines():
+            from_cave, to_cave = line.strip().split("-")
+            for from_cave, to_cave in [(from_cave, to_cave),  (to_cave, from_cave)]:
+                caves[from_cave] = caves.get(from_cave, []) + [to_cave]
+        print(calc(caves, 'start', {'start': 1}, False))
