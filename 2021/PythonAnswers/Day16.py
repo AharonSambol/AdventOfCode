e3463d3f572dabf27a2fb1710244d2869ad56ed8
AharonSambol <75334174+AharonSambol@users.noreply.github.com>
2021-12-17 14:16:53 +0200

    Update Day16.py

--- 2021/PythonAnswers/Day16.py
+++ 2021/PythonAnswers/Day16.py
@@ -2,9 +2,7 @@ import math
 
 
 def to_bin(st):
-    d = {'0': '0000', '1': '0001', '2': '0010', '3': '0011', '4': '0100', '5': '0101', '6': '0110', '7': '0111',
-         '8': '1000', '9': '1001', 'A': '1010', 'B': '1011', 'C': '1100', 'D': '1101', 'E': '1110', 'F': '1111'}
-    return list(''.join(d[x] for x in st))
+    return list(''.join(bin(int(x, 16))[2:].zfill(4) for x in st))
 
 
 def decode_packet(packet):
