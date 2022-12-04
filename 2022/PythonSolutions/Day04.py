e5ba2e58857d2bdfedc95688697a1793336d7050
Aharon Sambol <aharon.sambol@gmail.com>
2022-12-04 10:18:31 +0200

    day4

---
+++ 2022/PythonSolutions/Day04.py
@@ -0,0 +1,10 @@
+import re
+
+with open("../Inputs/InputDay04.txt") as file:
+    res1 = res2 = 0
+    for line in file:
+        (start1, end1, start2, end2) = map(int, re.findall(r'\d+', line))
+        res1 += start1 <= start2 <= end2 <= end1 or start2 <= start1 <= end1 <= end2
+        res2 += start1 <= start2 <= end1 or start1 <= end2 <= end1 or\
+            start2 <= start1 <= end2 or start2 <= end1 <= end2
+    print(res1, res2)
