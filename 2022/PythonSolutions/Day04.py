89f758afd72a71eb335dce700a183b956b2034ef
Aharon Sambol <aharon.sambol@gmail.com>
2022-12-04 10:55:56 +0200

    changed regex

--- 2022/PythonSolutions/Day04.py
+++ 2022/PythonSolutions/Day04.py
@@ -3,7 +3,8 @@ import re
 with open("../Inputs/InputDay04.txt") as file:
     res1 = res2 = 0
     for line in file:
-        start1, end1, start2, end2 = map(int, re.findall(r'\d+', line))
+        start1, end1, start2, end2 = map(int, re.split(r'[,-]', line))
+
         res1 += start1 <= start2 <= end2 <= end1 or start2 <= start1 <= end1 <= end2
         res2 += start1 <= start2 <= end1 or start2 <= start1 <= end2
     print(res1, res2)
--- 2022/rust_solutions/src/day04.rs
+++ 2022/rust_solutions/src/day04.rs
@@ -1,15 +1,15 @@
+use regex::Regex;
 use std::fs;
 
 pub fn solve() {
     let data = fs::read_to_string("../Inputs/InputDay04.txt").expect("Unable to read file");
     let (mut res1, mut res2) = (0, 0);
     for line in data.lines() {
-        let nums = line
-            .split(',')
-            .map(|x| x.split('-').map(|n| n.parse().unwrap()))
-            .into_iter()
-            .flatten()
-            .collect::<Vec<usize>>();
+        let nums: Vec<usize> = Regex::new(r"[,-]")
+            .unwrap()
+            .split(line)
+            .map(|x| x.parse().unwrap())
+            .collect();
         let (start1, end1, start2, end2) = (nums[0], nums[1], nums[2], nums[3]);
         res1 += (start1 <= start2 && end2 <= end1 || start2 <= start1 && end1 <= end2) as usize;
         res2 += (start1 <= start2 && start2 <= end1 || start2 <= start1 && start1 <= end2) as usize;
