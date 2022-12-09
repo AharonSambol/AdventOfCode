1766a860dbe4724701800cbc5c5e0feabad9898d
Aharon Sambol <aharon.sambol@gmail.com>
2022-12-09 11:00:04 +0200

    print both parts

--- 2022/PythonSolutions/Day09.py
+++ 2022/PythonSolutions/Day09.py
@@ -1,7 +1,6 @@
-part1 = False
 with open("../Inputs/InputDay09.txt") as file:
-    rope = [(0, 0)] * (2 if part1 else 10)
-    visited = {(0, 0)}
+    rope = [(0, 0)] * 10
+    visited2, visited1 = {(0, 0)}, {(0, 0)}
     for line in file:
         dr = {'R': (0, 1), 'L': (0, -1), 'D': (1, 0), 'U': (-1, 0)}[line[0]]
         for _ in range(int(line.split()[1])):
@@ -11,5 +10,6 @@ with open("../Inputs/InputDay09.txt") as file:
                 if abs(diff[0]) > 1 or abs(diff[1]) > 1:
                     diff = [x // max(1, abs(x)) for x in diff]
                     rope[k] = knot[0] + diff[0], knot[1] + diff[1]
-            visited.add(rope[-1])
-    print(len(visited))
+            visited1.add(rope[1])
+            visited2.add(rope[-1])
+    print(f'part1: {len(visited1)}, part2: {len(visited2)}')
