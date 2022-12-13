be34fa7ea274927eea481b47a79f665c7a76bc1b
Aharon Sambol <aharon.sambol@gmail.com>
2022-12-13 11:30:32 +0200

    used eval

--- 2022/PythonSolutions/Day13.py
+++ 2022/PythonSolutions/Day13.py
@@ -1,19 +1,6 @@
 import re
 from functools import cmp_to_key
 
-
-def parse(st):
-    path = [res := []]
-    for c in re.findall(r'(\d+|\[|])', st[1:-1]):
-        match c:
-            case '[':
-                path[-1].append([])
-                path.append(path[-1][-1])
-            case ']':   path.pop()
-            case _:     path[-1].append(int(c))
-    return res
-
-
 def compare(v1, v2):
     if type(v1) == type(v2) == int:
         return 0 if v1 == v2 else 1 if v1 < v2 else -1
@@ -27,8 +14,8 @@ with open("../Inputs/InputDay13.txt") as file:
     ipt = file.read()
     print(sum(i
               for i, lines in enumerate(ipt.split('\n\n'), start=1)
-              if compare(*map(parse, lines.split('\n'))) == 1))
+              if compare(*map(eval, lines.split('\n'))) == 1))
 
-    packets = [[[2]], [[6]]] + [parse(line) for line in re.split(r'\n+', ipt)]
+    packets = [[[2]], [[6]]] + [eval(line) for line in re.split(r'\n+', ipt)]
     packets.sort(key=cmp_to_key(compare), reverse=True)
     print((packets.index([[2]]) + 1) * (packets.index([[6]]) + 1))
