50613516d52c033cd8588054882ef118adc9917b
Aharon Sambol <aharon.sambol@gmail.com>
2022-12-20 12:24:33 +0200

    cleanup

--- 2022/PythonSolutions/Day20.py
+++ 2022/PythonSolutions/Day20.py
@@ -1,17 +1,11 @@
 part2 = True
-class Pointer:
-    def __init__(self, pos, orig):
-        self.pos = pos
-        self.orig = orig
-
 with open("../Inputs/InputDay20.txt") as file:
-    lst = [int(i) * (811589153 if part2 else 1) for i in file]
-    lst = [Pointer(x if x >= 0 else x % -(len(lst) - 1), x) for x in lst]
-    order = [x for x in lst]
+    lst = [(int(x) * (811589153 if part2 else 1), i) for i, x in enumerate(file)]
+    order = tuple(x for x in lst)
     for _ in range(10 if part2 else 1):
         for ct in range(len(lst)):
             i = lst.index(order[ct])
             val = lst.pop(i)
-            lst.insert((i + val.pos) % len(lst), val)
-    zero = next(i for i, x in enumerate(lst) if x.orig == 0)
-    print(sum(lst[(zero + 1000 * x) % len(lst)].orig for x in (1, 2, 3)))
+            lst.insert((i + val[0]) % len(lst), val)
+    zero = next(i for i, x in enumerate(lst) if x[0] == 0)
+    print(sum(lst[(zero + 1000 * x) % len(lst)][0] for x in (1, 2, 3)))
