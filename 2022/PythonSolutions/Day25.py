89d8d30c8b42f59f3f023bdbf4b37c72fe3c2d20
Aharon Sambol <aharon.sambol@gmail.com>
2022-12-25 21:22:43 +0200

    cleanup

--- 2022/PythonSolutions/Day25.py
+++ 2022/PythonSolutions/Day25.py
@@ -6,15 +6,8 @@ def add(n1, n2):
     for d1, d2 in zip(*map(lambda x: x[::-1].ljust(ln, '0'), (n1, n2))):
         d1, d2 = map(lambda x: '=-012'.index(x) - 2, (d1, d2))
         carry, add = divmod(d1 + d2 + carry, 5)
-        if add < -2:
-            add += 5
-            carry -= 1
-        if add > 2:
-            add = '=-'[add - 3]
-            carry += 1
-        else:
-            add = '=-012'[add + 2]
-        res.append(add)
+        carry += add // abs(add) if abs(add) > 2 else 0
+        res.append('=-012'[(add + 2) % 5])
     return ''.join(res[::-1]).lstrip('0')
 
 
