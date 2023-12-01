e58884715d83d6b6296d9645fd70b4aa975cad5c
Aharon Sambol <aharon.sambol@gmail.com>
2023-12-01 14:49:28 +0200

    cleanup
    Signed-off-by: Aharon Sambol <aharon.sambol@gmail.com>

--- 2023/python_solutions/day1.py
+++ 2023/python_solutions/day1.py
@@ -1,23 +1,15 @@
 import re
 
 
-def part1():
-    with open("../inputs/day01.txt") as file:
-        print(sum(int(x + y) for x, y in re.findall(r'.*?(?=(\d)).*(\d).*?', file.read())))
-
-
-def part2():
-    numbers = {
-        'one': '1', 'two': '2', 'three': '3', 'four': '4', 'five': '5',
-        'six': '6', 'seven': '7', 'eight': '8', 'nine': '9'
-    }
-    numbers_re = rf'(\d|{"|".join(numbers.keys())})'
+def solution(numbers):
+    numbers.update({str(x): x for x in range(1, 10)})
+    numbers_re = rf'({"|".join(numbers.keys())})'
     with open("../inputs/day01.txt") as file:
         print(sum(
-            int(numbers.get(x, x) + numbers.get(y, y))
+            numbers[x] * 10 + numbers[y]
             for x, y in re.findall(rf'.*?(?={numbers_re}).*{numbers_re}.*?', file.read())
         ))
 
 
-part1()
-part2()
+solution({})
+solution(dict(zip('one two three four five six seven eight nine'.split(), range(1, 10))))
