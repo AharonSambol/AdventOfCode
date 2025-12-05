b4a510e9ece3291ceafd3d3b3f85129c6fc9d8ad
AharonSambol <75334174+AharonSambol@users.noreply.github.com>
2025-12-05 12:19:27 +0200

    day5
    Signed-off-by: AharonSambol <75334174+AharonSambol@users.noreply.github.com>

---
+++ 2025/pythonSolutions/day5.py
@@ -0,0 +1,20 @@
+with (open("inputs/day5.txt") as f):
+    ranges, ingredients = f.read().split("\n\n")
+    print("part1:", sum(any(ingredient in range(*map(int, r.split("-"))) for r in ranges.split("\n")) for ingredient in (int(x) for x in ingredients.split("\n"))))
+
+    ranges, filtered_ranges = [], [[int(x) for x in rng.split("-")] for rng in ranges.split("\n")]
+    while len(ranges) != len(filtered_ranges):
+        ranges = filtered_ranges
+        filtered_ranges = []
+        for cur_range in ranges:
+            for i, other_range in enumerate(filtered_ranges):
+                if any(
+                        x in range(r1[0], r1[1] + 1)
+                        for r1, r2 in [(cur_range, other_range), (other_range, cur_range)]
+                        for x in r2
+                ):
+                    filtered_ranges[i] = [min(cur_range[0], other_range[0]), max(cur_range[1], other_range[1])]
+                    break
+            else:
+                filtered_ranges.append(cur_range)
+    print("part2:", sum(rng[1] - rng[0] + 1 for rng in filtered_ranges))
